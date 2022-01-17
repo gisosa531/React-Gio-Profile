de21bfbb4562b14ccc07f4c2e27ea7bc634c20ed
Giovanni Sosa <gio53196@gmail.com>
2022-01-17 00:40:11 -0800

    checking functionality of adding second project card in porftolio.js

--- gio-portfolio-react/src/components/Portfolio.js
+++ gio-portfolio-react/src/components/Portfolio.js
@@ -13,6 +13,7 @@ import {
 } from '@material-ui/core';
 import NavTabs from './NavTabs';
 import project1 from "../assets/projects/BlossomBabies.png";
+import project2 from "../assets/projects/Local4U2U.png";
 
 const style = makeStyles({
     mainCont: {
@@ -45,7 +46,7 @@ const Portfolio = () => {
 
                             <CardContent>
                                 <Typography gutterButton variant="h4">
-                                    Project 1
+                                    BlossomBabies
                                 </Typography>
                                 <Typography variant="body2" color="textSecondary" component="p">
                                     lorem ipsum dolor
@@ -61,6 +62,37 @@ const Portfolio = () => {
                             </CardActions>
                             </CardActionArea>
                             </Card>
+                           </Grid>
+{/* Project 2 */}
+<Grid item xs={12} sm={8} md={6}>
+                    <Card className= {classes.cardCont}>
+                        <CardActionArea>
+                            <CardMedia
+                            component="img"
+                            alt="Project 2"
+                            height="140"
+                            image={project2}
+                            />
+
+                            <CardContent>
+                                <Typography gutterButton variant="h4">
+                                    Local4U2U
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    lorem ipsum dolor
+                                </Typography>
+                            </CardContent>
+                            <CardActions size="small" color="primary">
+                                <Button>
+                                    Share
+                                </Button>
+                                <Button>
+                                    Live Demo
+                                </Button>
+                            </CardActions>
+                            </CardActionArea>
+                            </Card>
+
                             </Grid>
             </Grid>
         </Box>  
